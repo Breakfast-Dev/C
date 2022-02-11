fc10022286b862fa071667c75f2719391df6fa61
Breakfast-Dev <53260215+Breakfast-Dev@users.noreply.github.com>
2022-02-12 08:51:54 +0900

    fix

--- utils.h
+++ utils.h
@@ -1,33 +1,47 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#define class(name, pub, priv) \
-	typedef struct { pub } name ; \
-	typedef struct { name public; priv } name ## Pr ;
+#define pub
+#define priv
 
-#define constructor(parent, ...) \
-	parent * parent ## _new (__VA_ARGS__) { \
-		parent ## Pr *_this = (parent ## Pr *)malloc(sizeof(parent ## Pr)); \
-		parent *this = &_this->public;
+#define class(name, empty, pubc, privc) \
+	typedef struct name name;         	  \
+	typedef struct name##Pr name##Pr; 	  \
+	struct name                       	  \
+	{                               	    \
+		pubc                        		    \
+	};                             	     \
+	struct name##Pr               	      \
+	{                              	     \
+		name public;             		       \
+		privc                   		        \
+	};
 
-#define destructor(parent) \
-	void parent ## _delete(parent ## Pr * _this) { \
-		parent *this = &_this->public;
+#define constructor(parent, ...)                                   \
+	parent *parent##_new(__VA_ARGS__)                                \
+	{                                                                \
+		parent##Pr *_this = (parent##Pr *)malloc(sizeof(parent##Pr));  \
+		parent *this __attribute__((unused)) = &_this->public;
 
-#define method(parent, ...) \
-	(parent ## Pr *_this , ## __VA_ARGS__) { \
-		parent *this = &_this->public;
+#define destructor(parent)                 								 \
+	void parent##_delete(parent##Pr *_this)									 \
+	{                                       							    \
+		parent *this __attribute__((unused)) = &_this->public;
 
-#define init(this, ...) \
-	(this) = malloc(sizeof(*(this))); \
-	*(this) = (typeof(*(this))){ __VA_ARGS__ };
+#define method(parent, ...)             								 	 \
+	(parent##Pr * _this, ##__VA_ARGS__)										 	\
+	{                                  										   \
+		parent *this __attribute__((unused)) = &_this->public;
+
+#define init(this, ...)             			   \
+	*(this) = (typeof(*(this))){__VA_ARGS__};
 
 #define endm }
-#define endc \
-		return this; \
+#define endc  	  \
+		return this;  \
 	}
-#define endd \
-		free(_this); \
+#define endd 	   \
+		free(_this);  \
 	}
 
 #endif
