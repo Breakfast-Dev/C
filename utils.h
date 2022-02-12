38182688389ea55a0caf85e86b8180e24258d699
Breakfast-Dev <53260215+Breakfast-Dev@users.noreply.github.com>
2022-02-12 09:02:41 +0900

    fix

--- utils.h
+++ utils.h
@@ -5,43 +5,43 @@
 #define priv
 
 #define class(name, empty, pubc, privc) \
-	typedef struct name name;             \
-	typedef struct name##Pr name##Pr;     \
-	struct name                           \
-	{                                     \
-		pubc                                \
-	};                                    \
-	struct name##Pr                       \
-	{                                     \
-		name public;                        \
-		privc                               \
-	};
+  typedef struct name name;             \
+  typedef struct name##Pr name##Pr;     \
+  struct name                           \
+  {                                     \
+    pubc                                \
+  };                                    \
+  struct name##Pr                       \
+  {                                     \
+    name public;                        \
+    privc                               \
+  };
 
 #define constructor(parent, ...)                                   \
-	parent *parent##_new(__VA_ARGS__)                                \
-	{                                                                \
-		parent##Pr *_this = (parent##Pr *)malloc(sizeof(parent##Pr));  \
-		parent *this __attribute__((unused)) = &_this->public;
+  parent *parent##_new(__VA_ARGS__)                                \
+  {                                                                \
+    parent##Pr *_this = (parent##Pr *)malloc(sizeof(parent##Pr));  \
+    parent *this __attribute__((unused)) = &_this->public;
 
 #define destructor(parent)                                  \
-	void parent##_delete(parent##Pr *_this)                   \
-	{                                                         \
-		parent *this __attribute__((unused)) = &_this->public;
+  void parent##_delete(parent##Pr *_this)                   \
+  {                                                         \
+    parent *this __attribute__((unused)) = &_this->public;
 
 #define method(parent, ...)                                 \
-	(parent##Pr * _this, ##__VA_ARGS__)                       \
-	{                                                         \
-		parent *this __attribute__((unused)) = &_this->public;
+  (parent##Pr * _this, ##__VA_ARGS__)                       \
+  {                                                         \
+    parent *this __attribute__((unused)) = &_this->public;
 
 #define init(this, ...)                      \
-	*(this) = (typeof(*(this))){__VA_ARGS__};
+  *(this) = (typeof(*(this))){__VA_ARGS__};
 
 #define endm }
 #define endc      \
-		return this;  \
-	}
+    return this;  \
+  }
 #define endd      \
-		free(_this);  \
-	}
+    free(_this);  \
+  }
 
 #endif
